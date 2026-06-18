7b69c2c4739f9618291c0ad491985387117790e0
milk sheikh <nazar_zaman@outlook.com>
2026-06-18 12:33:53 +0000

    bruhhh

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+};
